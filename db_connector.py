e7ab03f60ba0cf630f9579e1940c5f5e61a5fc62
Sameer Mohamed <smrbasil4@gmail.com>
2019-06-19 20:53:34 +0530

    Imported the DB_Connector Script from the other Crawler

---
+++ db_connector.py
@@ -0,0 +1,91 @@
+from pymongo import MongoClient
+from pymongo.errors import ServerSelectionTimeoutError
+
+client = MongoClient('mongodb://forum_analyzer:admin123@ds157901.mlab.com:57901/moocrecv2')
+
+database = client.moocrecv2
+
+
+class Thread:
+
+    @staticmethod
+    def save_threads(threads):
+        try:
+            result = database.threads.insert(threads)
+            return result
+        except ServerSelectionTimeoutError:
+            print('Error Connecting to Database')
+        except:
+            print('An Error Occurred')
+
+    @staticmethod
+    def upsert_threads(threads):
+        try:
+            for thread in threads:
+                database.threads.update_one({'id': thread['id']}, {"$set": thread}, upsert=True)
+            return True
+        except ServerSelectionTimeoutError:
+            print('Error Connecting to Database')
+            return False
+        except:
+            print('An Error Occurred')
+            return False
+
+    @staticmethod
+    def get_discussion_threads_with_responses(course_id):
+        try:
+            results = database.threads.find(
+                {
+                    'course_id': course_id,
+                    'thread_type': 'discussion',
+                    '$or': [
+                        {'children': {'$exists': 'true'}},
+                        {'non_endorsed_responses': {'$exists': 'true'}}
+                    ]
+                }
+            ).limit(100)
+            return results
+        except ServerSelectionTimeoutError:
+            print('Error Connecting to Database')
+            return
+
+    @staticmethod
+    def get_sentiment_analyzed_threads():
+        try:
+            results = database.Threads.find({
+                'course_id': 'course-v1:UCSanDiegoX+DSE200x+1T2019a',
+                'thread_type': 'discussion',
+                '$or': [
+                    {'children': {'$exists': 'true'}},
+                    {'non_endorsed_responses': {'$exists': 'true'}}
+                ],
+                '$and': [{'is_sentiment_analyzed': {'$exists': 'true'}}, {'sentiment_score': {'$exists': 'true'}}]
+            }, {'is_sentiment_analyzed': 1, 'sentiment_score': 1}).sort({'sentiment_score': -1})
+            return results
+        except:
+            return []
+
+
+class Course:
+
+    @staticmethod
+    def upsert_courses(courses):
+        try:
+            for course in courses:
+                database.courses.update_one({'key': course['key']}, {"$set": course}, upsert=True)
+            return True
+        except ServerSelectionTimeoutError:
+            print('Error Connecting to Database')
+            return False
+        except:
+            print('An Error Occurred')
+            return False
+
+    @staticmethod
+    def get_course(course_key):
+        try:
+            courses = database.courses.find({'key': course_key})
+            return courses[0]
+        except:
+            return None
+            pass
